c8c465bb25dceafecd66c1a58d4434e2bbb6166f
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-12-23 21:46:42 -0800

    Updated develop.md
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 export TITLE="Homebridge"
 export DESCRIPTION="A dubo image for Homebridge"
 export IMAGE_NAME="homebridge"
-export PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7" # linux/arm/v6 is not supported for lack of node
+export PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64,linux/arm/v7}" # No v6 for lack of node
 
 # shellcheck source=/dev/null
 . "$(cd "$(dirname "${BASH_SOURCE[0]:-$PWD}")" 2>/dev/null 1>&2 && pwd)/helpers.sh"
